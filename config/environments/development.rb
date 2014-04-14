dbcda480630098c904be08c8f68b6d93c918f4f8
srikrishna <srikrishna>
2014-04-14 16:34:05 -0700

    ch13 complete

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,17 @@ Depot::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  #custom email settings
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "gmail.com",
+    authentication: "plain",
+    user_name: "test",
+    password: "test",
+    enable_starttls_auto: true
+  }
 end
