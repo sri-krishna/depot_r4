abb98b50c12791e87132bf9c11beb37bb881cf48
srikrishna <srikrishna>
2014-04-16 16:56:27 -0700

    ch15 completed

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -41,7 +41,7 @@ class OrdersController < ApplicationController
         Cart.destroy(session[:cart_id])
         session[:cart_id] = nil
         OrderNotifier.received(@order).deliver
-        format.html { redirect_to store_url, notice: 'Thank you for your order.' }
+        format.html { redirect_to store_url, notice: t('.thanks') }
         format.json { render action: 'show', status: :created, location: @order }
       else
         format.html { render action: 'new' }
--- config/routes.rb
+++ config/routes.rb
@@ -21,7 +21,14 @@ Depot::Application.routes.draw do
     get :who_bought, on: :member
   end
   
-  root 'store#index', as: 'store'
+  
+
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root 'store#index', as: 'store'  
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
